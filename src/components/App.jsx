9a72bd3b64265569176df99277e36d99b407c9ed
Jacob Hillock <jacob@hillock.cc>
2020-12-26 15:55:07 -0500

    Rewrite app as functional component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,6 @@
-import React, { Component, useContext } from "react";
+import React, { useContext } from "react";
 import Config from "./Config";
-// import Game from "./Game";
+import Game from "./Game";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import Container from "react-bootstrap/Container";
@@ -40,85 +40,31 @@ const AppNav = () => {
   );
 };
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      wins: [0, 0],
-      score: [0, 0],
-      initialServer: 0,
-      server: 0,
-      switchSides: false,
-      darkMode: false,
-    };
-    this.handlePointChange = this.handlePointChange.bind(this);
-    this.handleWinner = this.handleWinner.bind(this);
-    this.handleConfig = this.handleConfig.bind(this);
-  }
-  handlePointChange(score) {
-    this.setState((currentState) => ({
-      score,
-      server:
-        (Math.floor((score[0] + score[1]) / 2) + currentState.initialServer) %
-        2,
-    }));
-  }
-  handleWinner(wins) {
-    this.setState((currentState) => ({
-      score: [0, 0],
-      initialServer: (currentState.initialServer + 1) % 2,
-      server: (currentState.initialServer + 1) % 2,
-      wins,
-    }));
-  }
-  handleConfig(switchSides, darkMode) {
-    this.setState({
-      switchSides,
-      darkMode,
-    });
-  }
-  render() {
-    return (
-      <BrowserRouter>
-        <AppNav />
-        <Route
-          exact
-          path="/"
-          render={() => {
-            return <Home />;
-          }}
-        />
-        {/* <Route
-          path="/game"
-          render={() => {
-            return (
-              <Game
-                wins={this.state.wins}
-                score={this.state.score}
-                server={this.state.server}
-                onPointChange={this.handlePointChange}
-                onWinner={this.handleWinner}
-                switchSides={this.state.switchSides}
-              />
-            );
-          }}
-        /> */}
-        <Route
-          path="/config"
-          render={() => {
-            return (
-              <Config
-                switchSides={this.state.switchSides}
-                darkMode={this.state.darkMode}
-                onConfig={this.handleConfig}
-              />
-            );
-          }}
-        />
-      </BrowserRouter>
-    );
-  }
-}
+const App = () => {
+  return (
+    <BrowserRouter>
+      <AppNav />
+      <Route
+        exact
+        path="/"
+        render={() => {
+          return <Home />;
+        }}
+      />
+      <Route
+        path="/game"
+        render={() => {
+          return <Game />;
+        }}
+      />
+      <Route
+        path="/config"
+        render={() => {
+          return <Config />;
+        }}
+      />
+    </BrowserRouter>
+  );
+};
 
 export default App;
