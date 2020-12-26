f5f92fd4f413294ef097581e03aaad3130411a22
Jacob Hillock <jacob@hillock.cc>
2020-12-26 15:54:10 -0500

    Update provider to get all items in there

--- src/state/Provider.jsx
+++ src/state/Provider.jsx
@@ -3,11 +3,45 @@ import React, { useState } from "react";
 import Context from "./Context";
 
 const Provider = ({ children }) => {
-  const [players, setPlayers] = useState(["Player 1", "Player 2"]);
+  // Player state
+  const [players, setPlayers] = useState(
+    localStorage.getItem("players") === null
+      ? [0, 0]
+      : JSON.parse(localStorage.getItem("players"))
+  );
+  // Score state
+  const [scores, setScores] = useState(
+    sessionStorage.getItem("scores") === null
+      ? [0, 0]
+      : JSON.parse(sessionStorage.getItem("scores"))
+  );
+  // Win state
+  const [wins, setWins] = useState(
+    sessionStorage.getItem("wins") === null
+      ? [0, 0]
+      : JSON.parse(sessionStorage.getItem("wins"))
+  );
+
+  // Initial Server state
+  const [initialServer, setInitialServer] = useState(
+    JSON.parse(sessionStorage.getItem("initialServer")) || 0
+  );
+  const [switchSides, setSwitchSides] = useState(0);
+  const [darkMode, setDarkMode] = useState(0);
 
   const value = {
     players,
     setPlayers,
+    scores,
+    setScores,
+    wins,
+    setWins,
+    initialServer,
+    setInitialServer,
+    switchSides,
+    setSwitchSides,
+    darkMode,
+    setDarkMode,
   };
 
   return <Context.Provider value={value}>{children}</Context.Provider>;
